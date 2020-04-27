0ddc987f1dc797bb514e08a77bbf93207af39b22
Matias Alvarez <malvarez@viasatonline.it>
2020-04-27 17:30:03 +0200

    Updates

---
+++ precache-manifest.b470f2d2b370628d7ea78855ce82649a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c68465493649e6748b0695e8fd00f9fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63e940f78615f27d1aa0",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "661eb534acc980805b95",
+    "url": "/static/js/2.f310a501.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f310a501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63e940f78615f27d1aa0",
+    "url": "/static/js/main.bfbb95b4.chunk.js"
+  },
+  {
+    "revision": "fc67ddbfb20ab9c8f69e",
+    "url": "/static/js/runtime-main.eb1137c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
