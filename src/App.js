b5f4b443120c731b3820458aee458df882a644ee
Matias Alvarez <malvarez@viasatonline.it>
2020-04-27 20:50:06 +0200

    code for homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './pages/homepage/homepage.component'
+
 function App() {
-  return (
-    <div className="App">
-     <h1>E-commerce prototype</h1>
-     <h2>Alvarez Matias</h2>
-    </div>
-  );
+  return <div> 
+    
+    <HomePage/>
+
+  </div>;
 }
 
 export default App;
