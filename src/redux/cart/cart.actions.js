eb390c2d95ebcca6efb1833138f64e8ae6a1261e
Matias Alvarez <malvarez@viasatonline.it>
2020-05-05 12:28:20 +0200

    updated shop data + cart utils add item

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,9 @@ import CartActionTypes from './cart.types';
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN
 });
+
+//function addItems takes item as parameter and return an object for the reducer (action type + payload)
+export const addItem = item => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item
+})
